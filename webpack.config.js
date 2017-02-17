9ddd152efbaa92e7d4d93a7e1611fc8b6272eee0
devmpv <dev.mpv@gmail.com>
2017-02-17 22:53:04 +0300

    Cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-var nodeExternals = require('webpack-node-externals');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require('path');
 
@@ -12,8 +11,6 @@ module.exports = {
         path: __dirname,
         filename: './src/main/resources/static/built/bundle.js'
     },
-    //target: 'node', // in order to ignore built-in modules like path, fs, etc.
-    //externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
     module: {
         loaders: [
             {
@@ -33,9 +30,6 @@ module.exports = {
             { from: './src/main/resources/static/built', to: './target/classes/static/built' }
         ], {
             ignore: [],
-            // By default, we only copy modified files during
-            // a watch or webpack-dev-server build. Setting this
-            // to `true` copies all files.
             copyUnmodified: false
         })
     ]
